83a246f2a93ce03cd0eb6c81e14d572549f5c33c
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-04-29 18:38:34 +0200

    Add code for first and second part. Add appropriate compiler settings in ".vscode" in "tasks.json" & add .gitignore for the compiled files

---
+++ 1/faktorial.cpp
@@ -0,0 +1,322 @@
+
+
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+/*
+Zadani zde: https://ksp.mff.cuni.cz/h/ulohy/37/zadani1.html#task-37-1-S
+Vyrobil jsem zde zasobnikovy stroj,
+do ktereho jsem implementoval dostatecne instrukce na to, 
+aby v nich byl napsan program pocitajici faktorial.
+*/
+
+//misto #include <print> protoze mam stare g++ (13.2.0, kompilovano C++20 na linuxu)
+//potrebuje alespon C++20, ve VS Code uz by to melo byt nastavene v .vscode slozce v tasks.json
+#include <format>
+#include <iostream>
+using namespace std;
+template<class... Args>
+void print(format_string<Args...> fmt, Args&&... args){
+  cout << format(fmt, forward<Args>(args)...);
+}
+
+template<class... Args>
+void println(format_string<Args...> fmt, Args&&... args){
+  print(fmt, forward<Args>(args)...);
+  print("\n");
+}
+
+
+enum Opcode {
+  OP_PRINT,
+  OP_PUSH,
+
+  OP_LOAD,
+  OP_STORE,
+
+  OP_ADD,
+  OP_SUB,
+  OP_MUL,
+  OP_DIV,
+  OP_DUP,
+  OP_BRANCH,
+  OP_NOT,
+  OP_EQ,
+  OP_LT,
+  OP_NOP
+};
+
+struct Instruction {
+  //typ instrukce
+  Opcode op;
+
+  //nektere instrukce si potrebuji
+  //pamatovat dalsi hodnoty
+  //variant umoznuje mit v policku value ruzne typy: int, string, nebo monostate (= default placeholder)
+  //cilem potom je k tomu mit type safe moznost jak k tomu pristupovat
+  variant<monostate, int, string> value = monostate{};
+
+  //pak funkce std::get<string>(ins.value) z toho vezme string, pokud tam je, a jinak hodi error
+  //So, std::get<string>(ins.value) retrieves the string value stored in the ins.value variant, assuming that ins.value indeed holds a string.
+  //If ins.value does not contain a string, this will throw an exception of type std::bad_variant_access
+  
+};
+
+vector<Instruction> instrukce;
+vector<int> stack;
+unordered_map<string, int> promenne;
+int ip;
+
+
+
+
+using namespace std;
+
+void interpret(
+    vector<Instruction> const &instrukce,
+    vector<int> &zasobnik,
+    unordered_map<string, int> &promenne) {
+  int ip = 0; // index aktuální instrukce
+              // (instruction pointer)
+  while (true) {
+    if (!(ip >= 0 && ip < ssize(instrukce)))
+      break;
+
+    auto ins = instrukce.at(ip);
+
+    switch (ins.op) {
+    case OP_PRINT: {
+      println("PRINT: {}", zasobnik.back());
+    } break;
+
+    case OP_LOAD: {
+      //da hodnotu promenne .value (treba "x") na zasobnik
+      zasobnik.push_back(
+          promenne[get<string>(ins.value)]);
+
+    } break;
+
+    case OP_STORE: {
+      //da hodnotu ze zasobniku (pop) do promenne .value (treba "x")
+      //=popuje do promenne specifikovane v .value hodnotu z zasobniku (=pop zasobniku)
+      promenne[get<string>(ins.value)] = zasobnik.back();
+      zasobnik.pop_back();
+    } break;
+
+    case OP_PUSH: {
+      //da numerickou hodnotu (treba 8) na zasobnik
+      zasobnik.push_back(get<int>(ins.value));
+    } break;
+
+    case OP_ADD: {
+      //sebere 2 hodnoty ze zasobniku, a da na zasobnik jejich soucet
+      //pro debugging:
+      //pr pokud v zasobniku jenom jeden item, tak to muze popovat nahodny bordel
+      if(zasobnik.size() < 2){
+        cout << "Pozor, na zasobniku nejsou alespon dve hodnoty, OP_ADD!\n";
+        break;
+      }
+      int second = zasobnik.back();
+      //pop_back v C++ nevraci hodnotu returned elementu, proto je to rozdelene do zasobnik.back() a zasobnik.pop_back()
+      zasobnik.pop_back();
+      int first = zasobnik.back();
+      zasobnik.pop_back();
+
+      zasobnik.push_back(first + second);
+    } break;
+    case OP_SUB: {
+      //sebere 2 hodnoty ze zasobniku, a da na zasobnik jejich rozdil
+      //pr pokud v zasobniku jenom jeden item, tak to muze popovat nahodny bordel
+      if(zasobnik.size() < 2){
+        cout << "Pozor, na zasobniku nejsou alespon dve hodnoty, OP_SUB!\n";
+        break;
+      }
+      //  druhý operand byla první hodnota ze zásobníku a první operand druhá hodnota ze zásobníku
+      int second = zasobnik.back();
+      zasobnik.pop_back();
+      int first = zasobnik.back();
+      zasobnik.pop_back();
+      zasobnik.push_back(first - second);
+
+    } break;
+    case OP_MUL: {
+      // Implementuje integer multiplication
+      //pr pokud v zasobniku jenom jeden item, tak to muze popovat nahodny bordel
+      if(zasobnik.size() < 2){
+        cout << "Pozor, na zasobniku nejsou alespon dve hodnoty, OP_MUL!\n";
+        break;
+      }
+
+      int second = zasobnik.back();
+      zasobnik.pop_back();
+      int first = zasobnik.back();
+      zasobnik.pop_back();
+      zasobnik.push_back(second * first);
+    } break;
+    case OP_DIV: {
+      // Implementuje integer division (tedy zaukrouhleni dolu)
+      //pr pokud v zasobniku jenom jeden item, tak to muze popovat nahodny bordel
+      if(zasobnik.size() < 2){
+        cout << "Pozor, na zasobniku nejsou alespon dve hodnoty, OP_MUL!\n";
+        break;
+      }
+      int second = zasobnik.back();
+      zasobnik.pop_back();
+      int first = zasobnik.back();
+      zasobnik.pop_back();
+      if(second == 0){
+        cout << "Pozor, deleni 0\n";
+        return;
+      }
+      zasobnik.push_back(first / second);
+
+    } break;
+    case OP_DUP: {
+      // Odebere hodnotu ze zasobniku a hned ji tam 2x prida
+      // int val = zasobnik.back();
+      // zasobnik.pop_back();
+      // zasobnik.push_back(val);
+      // zasobnik.push_back(val);
+      //=nemusi ji vubec odebirat:
+      int val = zasobnik.back();
+      zasobnik.push_back(val);
+    } break;
+
+    case OP_BRANCH: {
+     int podminka = zasobnik.back();
+     zasobnik.pop_back();
+     if(podminka){
+      ip = get<int>(ins.value);
+     }
+
+    } break;
+
+    case OP_NOP : {
+        //Nedela nic
+        //abych nemusel prepisovat indexy vsech nasludujicih branchu kdyz chci "comment out" bloku pri debugovani
+        //tak ty instrukce nahradim NOP
+    } break;
+
+    case OP_NOT: {
+      //sebere cislo ze zasobniku, a z 0 udela 1, z 1 udela 0
+      if(zasobnik.size() == 0){
+        cout << "Pozor, na zasobniku neni zadna hodnota, OP_NOT\n";
+        break;
+      }
+      int value = zasobnik.back();
+      zasobnik.pop_back();
+      if(value == 1){
+        value = 0;
+      }else if (value == 0){
+        value = 1;
+      }
+      zasobnik.push_back(value);
+     
+    //tez funguje, chtelo se mi si trosku pohrat s C++
+    //   if(zasobnik.size() > 0){
+    //     //vector.back() can be used to assign a value to the last element of a vector
+    //     // => as it returns a reference to the last element. Better make sure it's not empty first! //https://stackoverflow.com/questions/13851367/can-vector-back-be-used-to-assign-a-value-to-the-last-element-of-a-vector
+    //     zasobnik.back() = !zasobnik.back();
+    //   }
+    } break;
+
+    case OP_EQ: {
+      //pr pokud v zasobniku jenom jeden item, tak to muze popovat nahodny bordel
+      if(zasobnik.size() < 2){
+        cout << "Pozor, na zasobniku nejsou alespon dve hodnoty, OP_EQ!\n";
+        break;
+      }
+      int a = zasobnik.back();
+      zasobnik.pop_back();
+      int b = zasobnik.back();
+      zasobnik.pop_back();
+      zasobnik.push_back(a == b);
+    } break;  
+
+    case OP_LT: {
+      //pr pokud v zasobniku jenom jeden item, tak to muze popovat nahodny bordel (a fun veci jako vector size -1, najednou se tam objevily itemy lol)
+      if(zasobnik.size() < 2){
+        cout << "Pozor, na zasobniku nejsou alespon dve hodnoty, OP_LT!\n";
+        break;
+      }
+      int first = zasobnik.back();
+      zasobnik.pop_back();
+      int second = zasobnik.back();
+      zasobnik.pop_back();
+      zasobnik.push_back(second < first);
+    } break;
+
+    }
+    ip += 1;
+  }
+}
+
+
+int main() {
+  vector<int> zasobnik;
+  vector<Instruction> instrukce(
+      {
+        //Do promenne x ulozime cislo, ktereho faktorial chceme spocitat
+        //vysledek bude po konci programu na stacku a vyprinti se
+        //pokud je x zaporne, nevypise se nic (nedefinovano matematicky, ze)
+        //v pripade nezaporneho x se vypise vysledek
+        //12 je s 32 bit inty nejvyssi cislo, kde to jeste nepretece
+        Instruction{.op = OP_PUSH, .value = 12}, //0 cislo radku
+        Instruction{.op = OP_STORE, .value = "x"}, //1
+
+        Instruction{.op = OP_LOAD, .value = "x"}, //2
+        Instruction{.op = OP_PUSH, .value = 0}, //3
+        Instruction{.op = OP_LT}, //4
+        Instruction{.op = OP_BRANCH, .value = 32}, //5
+
+        //jestli je x 1 return 1
+        Instruction{.op = OP_LOAD, .value = "x"}, //6
+        Instruction{.op = OP_PUSH, .value = 1}, //7
+        Instruction{.op = OP_EQ}, //8
+        Instruction{.op = OP_BRANCH, .value = 15}, //9
+        Instruction{.op = OP_NOP}, //10
+
+        //jestli je x 0 return 1
+        Instruction{.op = OP_LOAD, .value = "x"}, //11
+        Instruction{.op = OP_PUSH, .value = 0}, //12
+        Instruction{.op = OP_EQ}, //13
+        Instruction{.op = OP_NOT}, //14
+        Instruction{.op = OP_BRANCH, .value = 19}, //15
+        //tady je cast co provadi print 1
+        Instruction{.op = OP_PUSH, .value = 1}, //16
+        Instruction{.op = OP_PRINT}, //17
+        Instruction{.op = OP_PUSH, .value = 1}, //18
+        Instruction{.op = OP_BRANCH, .value = 32}, //19
+
+
+        //nacteni cisla z x na zasobnik (=protoze jeste na zasobniku nemame predchozi cislo, ktere budeme nasobit)
+        Instruction{.op = OP_LOAD, .value = "x"}, //20
+
+        //vypocet x-1 
+        Instruction{.op = OP_LOAD, .value = "x"}, //21
+        Instruction{.op = OP_PUSH, .value=1}, //22
+        Instruction{.op = OP_SUB}, //23
+
+        //x = x-1
+        Instruction{.op = OP_STORE, .value = "x"}, //24
+
+        Instruction{.op = OP_LOAD, .value = "x"}, //25
+        
+        Instruction{.op = OP_MUL}, //26
+        Instruction{.op = OP_NOP}, //27 //OP_PRINT
+
+        //if x > 1 jump up and repeat the loop
+        Instruction{.op = OP_PUSH, .value = 1}, //28
+        Instruction{.op = OP_LOAD, .value = "x"}, //29
+        Instruction{.op = OP_LT}, //30
+        Instruction{.op= OP_BRANCH, .value = 20}, //31
+        Instruction{.op= OP_PRINT}, //32
+
+      }); 
+
+  unordered_map<string, int> promenne;
+
+  interpret(instrukce, zasobnik, promenne);
+}
